14561c26d0d690a56fb1dfdf0dfe1536b0cf8bc7
kspica <s15990@pjwstk.edu.pl>
2024-11-09 13:41:30 +0100

    Zmmiana struktury katalogów

---
+++ zad1/src/main/java/Calculator.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public boolean addBoolean(boolean a, boolean b) {
+        return a && b;
+    }
+}
